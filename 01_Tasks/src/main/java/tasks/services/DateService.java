dbb3ee895e29ecf8c3550f4047ecf58ebed2ee99
ilies-bogdan <iliesbogdan147@gmail.com>
2024-03-27 22:57:54 +0200

    Lab2 BVA

--- 01_Tasks/src/main/java/tasks/services/DateService.java
+++ 01_Tasks/src/main/java/tasks/services/DateService.java
@@ -28,7 +28,12 @@ public class DateService {
         String[] units = time.split(":");
         int hour = Integer.parseInt(units[0]);
         int minute = Integer.parseInt(units[1]);
-        if (hour > HOURS_IN_A_DAY || minute > MINUTES_IN_HOUR) throw new IllegalArgumentException("time unit exceeds bounds");
+        if (hour >= HOURS_IN_A_DAY || minute >= MINUTES_IN_HOUR) {
+            throw new IllegalArgumentException("time unit exceeds bounds");
+        }
+        if (noTimeDate.before(new Date())) {
+            throw new IllegalArgumentException("invalid date");
+        }
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(noTimeDate);
         calendar.set(Calendar.HOUR_OF_DAY, hour);
--- 01_Tasks/src/test/java/tasks/services/DateServiceTest.java
+++ 01_Tasks/src/test/java/tasks/services/DateServiceTest.java
@@ -4,12 +4,25 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateServiceTest {
 
+    private DateService dateService;
+
+    Calendar calendar;
+
+    Date currentDate;
+
+
     @BeforeEach
     void setUp() {
+        dateService = new DateService();
+        calendar = Calendar.getInstance();
+        currentDate = new Date();
     }
 
     @AfterEach
@@ -17,10 +30,49 @@ class DateServiceTest {
     }
 
     @Test
-    void getLocalDateValueFromDate() {
+    void getDateMergedWithTime_BVA01() {
+        String time = "23:14";
+        calendar.add(Calendar.DATE, 7);
+        Date date = calendar.getTime();
+
+        Date actual = dateService.getDateMergedWithTime(time, date);
+
+        assert actual.after(currentDate);
+        assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
     }
 
     @Test
-    void getDateValueFromLocalDate() {
+    void getDateMergedWithTime_BVA02() {
+        String time = "12:60";
+        calendar.add(Calendar.DATE, 7);
+        Date date = calendar.getTime();
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> dateService.getDateMergedWithTime(time, date));
+
+        assert "time unit exceeds bounds".equals(ex.getMessage());
     }
+
+    @Test
+    void getDateMergedWithTime_BVA03() {
+        String time = "21:53";
+        calendar.add(Calendar.DATE, 1);
+        Date date = calendar.getTime();
+
+        Date actual = dateService.getDateMergedWithTime(time, date);
+
+        assert actual.after(currentDate);
+        assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
+    }
+
+    @Test
+    void getDateMergedWithTime_BVA04() {
+        String time = "21:53";
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> dateService.getDateMergedWithTime(time, currentDate));
+
+        assert "invalid date".equals(ex.getMessage());
+    }
+
 }
