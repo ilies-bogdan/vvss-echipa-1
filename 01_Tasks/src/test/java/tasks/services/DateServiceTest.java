762195a1f18dae758dd1c1a09d2c8df3afdfcbae
bogdan.ilies <bogdan.ilies@accesa.eu>
2024-03-28 10:08:59 +0200

    Add Junit annotations

--- 01_Tasks/src/test/java/tasks/services/DateServiceTest.java
+++ 01_Tasks/src/test/java/tasks/services/DateServiceTest.java
@@ -2,7 +2,13 @@ package tasks.services;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -13,10 +19,9 @@ class DateServiceTest {
 
     private DateService dateService;
 
-    Calendar calendar;
+    private Calendar calendar;
 
-    Date currentDate;
-
+    private Date currentDate;
 
     @BeforeEach
     void setUp() {
@@ -29,50 +34,62 @@ class DateServiceTest {
     void tearDown() {
     }
 
-    @Test
-    void getDateMergedWithTime_BVA01() {
-        String time = "23:14";
-        calendar.add(Calendar.DATE, 7);
-        Date date = calendar.getTime();
+    @Nested
+    class DateServiceTestBVA {
 
-        Date actual = dateService.getDateMergedWithTime(time, date);
+        @RepeatedTest(3)
+        @DisplayName("BVA01")
+        void getDateMergedWithTime_BVA01() {
+            String time = "23:14";
+            calendar.add(Calendar.DATE, 7);
+            Date date = calendar.getTime();
 
-        assert actual.after(currentDate);
-        assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
-    }
+            Date actual = dateService.getDateMergedWithTime(time, date);
 
-    @Test
-    void getDateMergedWithTime_BVA02() {
-        String time = "12:60";
-        calendar.add(Calendar.DATE, 7);
-        Date date = calendar.getTime();
+            assert actual.after(currentDate);
+            assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
+        }
 
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
-                () -> dateService.getDateMergedWithTime(time, date));
+        @Test
+        @DisplayName("BVA02")
+        @Timeout(2)
+        void getDateMergedWithTime_BVA02() {
+            String time = "12:60";
+            calendar.add(Calendar.DATE, 7);
+            Date date = calendar.getTime();
 
-        assert "time unit exceeds bounds".equals(ex.getMessage());
-    }
+            IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                    () -> dateService.getDateMergedWithTime(time, date));
 
-    @Test
-    void getDateMergedWithTime_BVA03() {
-        String time = "21:53";
-        calendar.add(Calendar.DATE, 1);
-        Date date = calendar.getTime();
+            assert "time unit exceeds bounds".equals(ex.getMessage());
+        }
 
-        Date actual = dateService.getDateMergedWithTime(time, date);
+        @ParameterizedTest
+        @ValueSource(strings = {"21:53"})
+        @DisplayName("BVA03")
+        void getDateMergedWithTime_BVA03(String time) {
+            calendar.add(Calendar.DATE, 1);
+            Date date = calendar.getTime();
 
-        assert actual.after(currentDate);
-        assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
-    }
+            Date actual = dateService.getDateMergedWithTime(time, date);
 
-    @Test
-    void getDateMergedWithTime_BVA04() {
-        String time = "21:53";
+            assert actual.after(currentDate);
+            assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
+        }
 
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
-                () -> dateService.getDateMergedWithTime(time, currentDate));
+        @Test
+        @DisplayName("BVA04")
+        void getDateMergedWithTime_BVA04() {
+            String time = "21:53";
+            calendar.add(Calendar.DATE, -1);
+            Date date = calendar.getTime();
 
-        assert "invalid date".equals(ex.getMessage());
+            IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                    () -> dateService.getDateMergedWithTime(time, date));
+
+            assert "invalid date".equals(ex.getMessage());
+        }
+
     }
 
 }
