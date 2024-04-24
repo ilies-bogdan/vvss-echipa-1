8d20919f0927b470af1c40a6db5fb1597ccc216b
ilies-bogdan <iliesbogdan147@gmail.com>
2024-04-24 22:12:48 +0300

    schema

--- src/test/java/tasks/model/TaskTest.java
+++ src/test/java/tasks/model/TaskTest.java
@@ -40,88 +40,84 @@ public class TaskTest {
     public void tearDown() {
     }
 
-    @Nested
-    public class NextTimeAfterWBT {
+    @Test
+    public void testNextTimeAfterF02_TC01() throws ParseException {
+        task = new Task("Task F02_TC01", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
+        task.setActive(true);
+        Date current = SIMPLE_DATE_FORMAT.parse("18.04.2024");
+        assertNull(task.nextTimeAfter(current));
+    }
 
-        @Test
-        public void testNextTimeAfterF02_TC01() throws ParseException {
-            task = new Task("Task F02_TC01", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
-            task.setActive(true);
-            Date current = SIMPLE_DATE_FORMAT.parse("18.04.2024");
-            assertNull(task.nextTimeAfter(current));
-        }
+    @Test
+    public void testNextTimeAfterF01_TC02() throws ParseException {
+        task = new Task("Task F01_TC02", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
+        task.setTime(SIMPLE_DATE_FORMAT.parse("16.04.2024"));
+        task.setActive(true);
+        Date current = SIMPLE_DATE_FORMAT.parse("12.04.2024");
+        Date expected = SIMPLE_DATE_FORMAT.parse("16.04.2024");
+        assertEquals(expected, task.nextTimeAfter(current));
+    }
 
-        @Test
-        public void testNextTimeAfterF01_TC02() throws ParseException {
-            task = new Task("Task F01_TC02", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
-            task.setTime(SIMPLE_DATE_FORMAT.parse("16.04.2024"));
-            task.setActive(true);
-            Date current = SIMPLE_DATE_FORMAT.parse("12.04.2024");
-            Date expected = SIMPLE_DATE_FORMAT.parse("16.04.2024");
-            assertEquals(expected, task.nextTimeAfter(current));
-        }
+    @Test
+    public void testNextTimeAfterF01_TC03() throws ParseException {
+        task = new Task("Task F01_TC03", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
+        task.setActive(true);
+        task.setTime(SIMPLE_DATE_FORMAT.parse("16.04.2024"));
+        Date current = SIMPLE_DATE_FORMAT.parse("16.04.2024");
+        assertNull(task.nextTimeAfter(current));
+    }
 
-        @Test
-        public void testNextTimeAfterF01_TC03() throws ParseException {
-            task = new Task("Task F01_TC03", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
-            task.setActive(true);
-            task.setTime(SIMPLE_DATE_FORMAT.parse("16.04.2024"));
-            Date current = SIMPLE_DATE_FORMAT.parse("16.04.2024");
-            assertNull(task.nextTimeAfter(current));
-        }
+    @Test
+    public void testNextTimeAfterF01_TC04() throws ParseException {
+        task = new Task("Task F01_TC04", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
+        task.setActive(true);
+        Date current = SIMPLE_DATE_FORMAT.parse("10.04.2024");
+        Date expected = SIMPLE_DATE_FORMAT.parse("11.04.2024");
+        assertEquals(expected, task.nextTimeAfter(current));
+    }
 
-        @Test
-        public void testNextTimeAfterF01_TC04() throws ParseException {
-            task = new Task("Task F01_TC04", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
-            task.setActive(true);
-            Date current = SIMPLE_DATE_FORMAT.parse("10.04.2024");
-            Date expected = SIMPLE_DATE_FORMAT.parse("11.04.2024");
-            assertEquals(expected, task.nextTimeAfter(current));
-        }
+    @Test
+    public void testNextTimeAfterF01_TC05() throws ParseException {
+        task = new Task("Task F01_TC05", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
+        task.setActive(true);
+        Date current = SIMPLE_DATE_FORMAT.parse("12.04.2024");
 
-        @Test
-        public void testNextTimeAfterF01_TC05() throws ParseException {
-            task = new Task("Task F01_TC05", SIMPLE_DATE_FORMAT.parse("11.04.2024"), SIMPLE_DATE_FORMAT.parse("15.04.2024"), 1);
-            task.setActive(true);
-            Date current = SIMPLE_DATE_FORMAT.parse("12.04.2024");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(current);
+        calendar.add(Calendar.SECOND, 1);
+        Date expected = calendar.getTime();
+        assertEquals(expected, task.nextTimeAfter(current));
+    }
 
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(current);
-            calendar.add(Calendar.SECOND, 1);
-            Date expected = calendar.getTime();
-            assertEquals(expected, task.nextTimeAfter(current));
-        }
+    @Test
+    public void testNextTimeAfterF02_TC06() throws ParseException {
+        Date start = SIMPLE_DATE_FORMAT.parse("10.04.2024");
+        Date end = SIMPLE_DATE_FORMAT.parse("18.04.2024");
+        Date current = SIMPLE_DATE_FORMAT.parse("15.04.2024");
+        task = new Task("Test Task", start, end, 86400);
+        current.setTime(current.getTime() + (task.getRepeatInterval() / 2) * 1000);
 
-        @Test
-        public void testNextTimeAfterF02_TC06() throws ParseException {
-            Date start = SIMPLE_DATE_FORMAT.parse("10.04.2024");
-            Date end = SIMPLE_DATE_FORMAT.parse("18.04.2024");
-            Date current = SIMPLE_DATE_FORMAT.parse("15.04.2024");
-            task = new Task("Test Task", start, end, 86400);
-            current.setTime(current.getTime() + (task.getRepeatInterval() / 2) * 1000);
+        Date nextTime = task.nextTimeAfter(current);
 
-            Date nextTime = task.nextTimeAfter(current);
-
-            assertNull(nextTime);
-        }
+        assertNull(nextTime);
+    }
 
 
-        @Test
-        public void testNextTimeAfterF02TC07() throws ParseException {
-            Date start = SIMPLE_DATE_FORMAT.parse("10.04.2024");
-            Date end = SIMPLE_DATE_FORMAT.parse("18.04.2024");
+    @Test
+    public void testNextTimeAfterF02TC07() throws ParseException {
+        Date start = SIMPLE_DATE_FORMAT.parse("10.04.2024");
+        Date end = SIMPLE_DATE_FORMAT.parse("18.04.2024");
 
-            Date current = SIMPLE_DATE_FORMAT.parse("15.04.2024");
-            task = new Task("Test Task", start, end, 86400);
-            task.setActive(true);
+        Date current = SIMPLE_DATE_FORMAT.parse("15.04.2024");
+        task = new Task("Test Task", start, end, 86400);
+        task.setActive(true);
 
 
-            Date nextTime = task.nextTimeAfter(current);
+        Date nextTime = task.nextTimeAfter(current);
 
-            Date expectedNextTime = SIMPLE_DATE_FORMAT.parse("16.04.2024");
+        Date expectedNextTime = SIMPLE_DATE_FORMAT.parse("16.04.2024");
 
-            assertEquals(expectedNextTime, nextTime);
-        }
+        assertEquals(expectedNextTime, nextTime);
     }
 
 
--- src/test/java/tasks/services/DateServiceTest.java
+++ src/test/java/tasks/services/DateServiceTest.java
@@ -34,111 +34,103 @@ public class DateServiceTest {
     public void tearDown() {
     }
 
-    @Nested
-    public class DateServiceTestBVA {
+    @RepeatedTest(3)
+    @DisplayName("BVA01")
+    public void getDateMergedWithTime_BVA01() {
+        String time = "23:14";
+        calendar.add(Calendar.DATE, 7);
+        Date date = calendar.getTime();
 
-        @RepeatedTest(3)
-        @DisplayName("BVA01")
-        public void getDateMergedWithTime_BVA01() {
-            String time = "23:14";
-            calendar.add(Calendar.DATE, 7);
-            Date date = calendar.getTime();
+        Date actual = dateService.getDateMergedWithTime(time, date);
 
-            Date actual = dateService.getDateMergedWithTime(time, date);
-
-            assert actual.after(currentDate);
-            assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
-        }
-
-        @Test
-        @DisplayName("BVA02")
-        @Timeout(2)
-        public void getDateMergedWithTime_BVA02() {
-            String time = "12:60";
-            calendar.add(Calendar.DATE, 7);
-            Date date = calendar.getTime();
-
-            IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
-                    () -> dateService.getDateMergedWithTime(time, date));
-
-            assert "time unit exceeds bounds".equals(ex.getMessage());
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = {"21:53"})
-        @DisplayName("BVA03")
-        public void getDateMergedWithTime_BVA03(String time) {
-            calendar.add(Calendar.DATE, 1);
-            Date date = calendar.getTime();
-
-            Date actual = dateService.getDateMergedWithTime(time, date);
-
-            assert actual.after(currentDate);
-            assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
-        }
-
-        @Test
-        @DisplayName("BVA04")
-        public void getDateMergedWithTime_BVA04() {
-            String time = "21:53";
-            calendar.add(Calendar.DATE, -1);
-            Date date = calendar.getTime();
-
-            IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
-                    () -> dateService.getDateMergedWithTime(time, date));
-
-            assert "invalid date".equals(ex.getMessage());
-        }
-
+        assert actual.after(currentDate);
+        assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
     }
 
-    @Nested
-    public class DateServiceTestECP {
-        @Test
-        @DisplayName("ECP01")
-        public void testValidTimeFutureDate() {
-            String time = "10:30";
-            Date futureDate = new Date(2025, Calendar.FEBRUARY, 28);
+    @Test
+    @DisplayName("BVA02")
+    @Timeout(2)
+    public void getDateMergedWithTime_BVA02() {
+        String time = "12:60";
+        calendar.add(Calendar.DATE, 7);
+        Date date = calendar.getTime();
 
-            Date mergedDate = dateService.getDateMergedWithTime(time, futureDate);
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> dateService.getDateMergedWithTime(time, date));
 
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(futureDate);
-            calendar.set(Calendar.HOUR_OF_DAY, 10);
-            calendar.set(Calendar.MINUTE, 30);
+        assert "time unit exceeds bounds".equals(ex.getMessage());
+    }
 
-            assertEquals(calendar.getTime(), mergedDate);
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {"21:53"})
+    @DisplayName("BVA03")
+    public void getDateMergedWithTime_BVA03(String time) {
+        calendar.add(Calendar.DATE, 1);
+        Date date = calendar.getTime();
 
-        @Test
-        @DisplayName("ECP02")
-        public void testInvalidTimeFormat() {
-            String invalidTime = "invalid";
-            Date anyDate = new Date();
+        Date actual = dateService.getDateMergedWithTime(time, date);
 
-            assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(invalidTime, anyDate));
-        }
+        assert actual.after(currentDate);
+        assert Math.abs(date.getTime() - actual.getTime()) < 24 * 60 * 60 * 1000;
+    }
 
-        @Test
-        @DisplayName("ECP03")
-        public void testTimeUnitExceedingBounds() {
-            String timeExceedingHour = "25:00";
-            String timeExceedingMinute = "10:65";
-            Date anyDate = new Date();
+    @Test
+    @DisplayName("BVA04")
+    public void getDateMergedWithTime_BVA04() {
+        String time = "21:53";
+        calendar.add(Calendar.DATE, -1);
+        Date date = calendar.getTime();
 
-            assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(timeExceedingHour, anyDate));
-            assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(timeExceedingMinute, anyDate));
-        }
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> dateService.getDateMergedWithTime(time, date));
 
-        @Test
-        @DisplayName("ECP04")
-        public void testPastDate() {
-            String time = "10:30";
-            Calendar calendar = Calendar.getInstance();
-            calendar.add(Calendar.DATE, -1);
-            Date pastDate = calendar.getTime();
+        assert "invalid date".equals(ex.getMessage());
+    }
 
-            assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(time, pastDate));
-        }
+    @Test
+    @DisplayName("ECP01")
+    public void testValidTimeFutureDate() {
+        String time = "10:30";
+        Date futureDate = new Date(2025, Calendar.FEBRUARY, 28);
+
+        Date mergedDate = dateService.getDateMergedWithTime(time, futureDate);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(futureDate);
+        calendar.set(Calendar.HOUR_OF_DAY, 10);
+        calendar.set(Calendar.MINUTE, 30);
+
+        assertEquals(calendar.getTime(), mergedDate);
+    }
+
+    @Test
+    @DisplayName("ECP02")
+    public void testInvalidTimeFormat() {
+        String invalidTime = "invalid";
+        Date anyDate = new Date();
+
+        assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(invalidTime, anyDate));
+    }
+
+    @Test
+    @DisplayName("ECP03")
+    public void testTimeUnitExceedingBounds() {
+        String timeExceedingHour = "25:00";
+        String timeExceedingMinute = "10:65";
+        Date anyDate = new Date();
+
+        assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(timeExceedingHour, anyDate));
+        assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(timeExceedingMinute, anyDate));
+    }
+
+    @Test
+    @DisplayName("ECP04")
+    public void testPastDate() {
+        String time = "10:30";
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -1);
+        Date pastDate = calendar.getTime();
+
+        assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(time, pastDate));
     }
 }
